b527d8029278f238d5f195bf92863ba9a40af9b8
AYDEN HOLGATE <AYDEN.HOLGATE@connect.7oaks.org>
2021-01-21 14:17:46 -0600

    Initial commit

---
+++ divisibleAyden/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnBegin = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnBegin
+        '
+        Me.btnBegin.Location = New System.Drawing.Point(105, 119)
+        Me.btnBegin.Name = "btnBegin"
+        Me.btnBegin.Size = New System.Drawing.Size(75, 23)
+        Me.btnBegin.TabIndex = 0
+        Me.btnBegin.Text = "Begin"
+        Me.btnBegin.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnBegin)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnBegin As System.Windows.Forms.Button
+
+End Class
---
+++ divisibleAyden/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+
+    Private Sub btnBegin_Click(sender As Object, e As EventArgs) Handles btnBegin.Click
+        Dim intDivisor As Integer
+        Dim intNumber As Integer
+        Dim intCount As Integer = -1
+        Const FLAG As Integer = 0
+
+        Do
+            intDivisor = InputBox("Enter A Positive Number Or 0 To Quit")
+
+            If intDivisor < 0 Then
+                MessageBox.Show("ERROR! Enter A Positive Number")
+            End If
+        Loop Until intDivisor > 0
+
+        Do
+            intNumber = InputBox("Enter A Positive Number Or 0 To Quit")
+
+            If intNumber Mod intDivisor = 0 Then
+                intCount = intCount + 1
+            End If
+
+        Loop Until intNumber = FLAG
+
+        MessageBox.Show("You entered " & intCount & " number(s) that are divisible by " & intDivisor)
+
+    End Sub
+End Class
